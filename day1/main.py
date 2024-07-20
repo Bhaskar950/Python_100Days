e1a2a79a1f7ec49385a134d6d042adde3cfc4473
Bhaskar950 <koilada9.dev@gmail.com>
2024-07-20 20:56:55 +0530

    python day1

---
+++ day1/main.py
@@ -0,0 +1,41 @@
+# print('Hello World!!\n im bhaskar')
+
+# print('Hello' + " " + 'bhaskar')
+
+# print("hello " + input("Type your name"))
+# age = input('Enter your age: ')
+# print('Your age is' + age)
+
+
+# name = 'bhaskar9fghthghghghghghg9'
+# print(name)
+# length = (len(name))
+# print(length)
+
+
+# a = input()
+
+# b = input()
+
+# c = a
+# a = b
+# b = c
+# # 
+
+# print('a: ' + a)
+# print('b: ' + b) 
+
+
+# i = input('tell me your name ')
+
+# l = len(i)
+
+# print(l)
+
+
+
+print('Welcome to band name generator')
+
+user_city = input('Enter your city you grew up in ')
+user_pet = input('Enter your pet name ')
+band_name = print(user_city,user_pet)
