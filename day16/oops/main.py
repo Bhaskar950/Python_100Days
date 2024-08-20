3c5270db8616c629f40315fd2450a3c4a012ee6e
Bhaskar950 <koilada9.dev@gmail.com>
2024-08-20 16:00:48 +0530

    coffee machine with opps

--- day16/oops/main.py
+++ day16/oops/main.py
@@ -1,5 +1,22 @@
-from prettytable import PrettyTable
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+money_machine.report()
+# print(money_machine)
+
+
+# from prettytable import PrettyTable
+#
+# table = PrettyTable()
+#
+# table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+# table.add_column("Type",["Electric","water","Fire"])
+# table.align = "l"
+# print(table)
 
 
 
