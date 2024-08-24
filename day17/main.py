d6441f97702c7454f62e1091c06d574a123861fa
Bhaskar950 <koilada9.dev@gmail.com>
2024-08-24 18:01:29 +0530

    brain_quiz

---
+++ day17/main.py
@@ -0,0 +1,18 @@
+
+class User:
+
+    def __init__(self):
+        print('new user has been created')
+
+
+
+
+user = User()
+
+user.name = "bhaskar"
+user.id = "01"
+
+print(user.name)
+print(user.id)
+
+# user2 = User()
