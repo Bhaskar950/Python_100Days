a3443196fa6bf131b90a039343a4d24d01ee8a4b
Bhaskar950 <koilada9.dev@gmail.com>
2024-08-30 12:51:00 +0530

    turtle random walk with random colors

--- day18/Turtle_project/main.py
+++ day18/Turtle_project/main.py
@@ -1,9 +1,11 @@
 from turtle import Turtle , Screen
+import turtle as t
 import random
 
 
 tim = Turtle()
 tim.shape("turtle")
+t.colormode(255)
 
 
 # for _ in range(15):
@@ -28,12 +30,22 @@ colors = ["DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen",
 #     tim.color(random.choice(colors))
 #     draw_shape(shape_side_n)
 
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    clr = (r,g,b)
+    return clr
+
 directions = [0,90,180,270]
 tim.pensize(20)
-tim.speed("fastest")
+tim.speed("slow")
+
+
 
 for _ in range(200):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
